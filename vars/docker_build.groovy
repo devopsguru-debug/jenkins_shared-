724b43f3359dac1af4fc7dc42008596d699eb92c
devopsguru-debug <sahil.pujara@bonamisoftware.com>
2024-12-05 12:21:53 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(String Image, String Imagetag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${Image}:${Imagetag}"
+  sh "docker build -t ${DockerHubUser}/${Image}:${Imagetag} ."
 }
