114ce07a99679e6668d32fdd9d83409f8f26c030
devopsguru-debug <sahil.pujara@bonamisoftware.com>
2024-12-05 12:25:28 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,4 @@
-def call(String Image, String Imagetag, String DockerHubUser){
+def call(String Image, String Imagetag, String dockerhubUser){
   withCredentials([usernamePassword(credentialsId: 'dockerhubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
                   sh "docker login -u ${dockerhubUser} -p ${dockerhubPass}"
                   }
